a9d54092d0278333f129cd79fbe1a95e5f73435d
viktorbjo <90506608+viktorbjo@users.noreply.github.com>
2023-01-20 16:13:18 +0100

    Update number_of_sibling.sql

--- sql/s3/number_of_sibling.sql
+++ sql/s3/number_of_sibling.sql
@@ -6,3 +6,15 @@ WITH distinct_siblings AS (
 SELECT student_id, COUNT(student_siblings_id) AS num_siblings
 FROM distinct_siblings
 GROUP BY student_id;
+
+-- ////////////////////////
+
+select sibling.siblings as nr_of_siblings, COUNT(*) as nr_of_students
+from (select id, COUNT(student_id) siblings
+from student
+left join siblings s on student.id = s.student_id
+group by id
+order by id) as sibling
+group by siblings
+order by nr_of_siblings;
+
